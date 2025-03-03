e372cd015bfb8fe87dbef84293d6d257adea112d
eding <edin.grech@gmail.com>
2025-03-03 15:37:56 +0100

    feat: made model fillable & added cast

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'dob',
+        'college_id',
+    ];
+
+    protected $casts = [
+        'dob' => 'date',
+    ];
+
+
+    public function college()
+    {
+        return $this->belongsTo(College::class);
+    }
 }
